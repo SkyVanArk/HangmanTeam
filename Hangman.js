bf6225d6bdf2cb8f65828474796ac1110972f163
Schuyler Van Ark <schuyler.vanark@gmail.com>
2017-11-20 19:35:59 -0700

    DOOMED

---
+++ Hangman.js
@@ -0,0 +1,43 @@
+/*
+ * H A N G M A N
+ * Collaborative Development Edition
+ */ 
+
+var easy = {
+  wordbank: [],
+  numGuesses: 15
+};
+
+var medium = {
+  wordbank: [],
+  numGuesses: 12,
+};
+
+var hard = {
+  wordbank: [],
+  numGuesses: 9,
+};
+
+var display = {
+	
+};
+
+var logic = {
+
+};
+
+var gameOver = {
+	win(){
+
+	};
+	lose(){
+		
+	};
+};
+
+var selectedMode="";
+var wins=0;
+var losses=0;
+var activeWord="";
+var wrongGuesses=[];
+
